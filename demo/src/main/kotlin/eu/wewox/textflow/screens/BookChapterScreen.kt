655dd09d622855e3df22aa963150e50093826c65
w2sv <zangenbergjanek@googlemail.com>
2024-06-18 21:36:09 +0200

    Migrate demo

--- demo/src/main/kotlin/eu/wewox/textflow/screens/BookChapterScreen.kt
+++ demo/src/main/kotlin/eu/wewox/textflow/screens/BookChapterScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -72,18 +73,18 @@ fun BookChapterScreen() {
 @Composable
 @Suppress("NOTHING_TO_INLINE") // To not create a scope
 private inline fun rememberTextIndex(text: String): State<Int> {
-    val index = remember { mutableStateOf(1) }
+    val index = remember { mutableIntStateOf(1) }
     LaunchedEffect(Unit) {
         val range = 1 until text.length
         while (isActive) {
             range.forEach {
                 delay(TextTypeDelay)
-                index.value = it
+                index.intValue = it
             }
             delay(TextStillDelay)
             range.reversed().forEach {
                 delay(TextTypeDelay)
-                index.value = it
+                index.intValue = it
             }
         }
     }
